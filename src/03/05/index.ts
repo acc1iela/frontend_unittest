d6f98e91ffa8a9587e45d19e29364148509797ec
acc1iela <acc1iela@gmail.com>
2023-05-14 23:00:09 +0900

    update: add関数を修正

--- src/03/05/index.ts
+++ src/03/05/index.ts
@@ -2,18 +2,32 @@ export class RangeError extends Error {}
 
 function checkRange(value: number) {
   if (value < 0 || value > 100) {
-    throw new RangeError("入力値は0〜100の間で入力してください");
+    throw new RangeError('入力値は0〜100の間で入力してください');
   }
 }
 
+// export function add(a: number, b: number) {
+//   checkRange(a);
+//   checkRange(b);
+//   const sum = a + b;
+//   if (sum > 100) {
+//     return 100;
+//   }
+//   return sum;
+// }
+
 export function add(a: number, b: number) {
-  checkRange(a);
-  checkRange(b);
+  if (a < 0 || b > 100) {
+    throw new RangeError('入力値は0〜100の間で入力してください');
+  }
+  if (b < 0 || b > 100) {
+    throw new Error('入力値は0~100の間で入力してください');
+  }
   const sum = a + b;
   if (sum > 100) {
     return 100;
   }
-  return sum;
+  return a + b;
 }
 
 export function sub(a: number, b: number) {
